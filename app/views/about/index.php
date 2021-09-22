3a6e048c0bcbc01364e5005b6cc8c59453793f2d
bagasarya15 <bagasarya150800@gmail.com>
2021-09-23 01:12:22 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container ">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/PF.jpg" alt="Bagas Arya" width="140" class="rounded-circle shadow">
+    <p class="mt-2">Hallo, Nama saya <?=$data['nama'];?>, Umur saya <?=$data['umur'];?> Tahun, Saya adalah seorang <?=$data['pekerjaan'];?>.</p>
+</div>
---
+++ app/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+    <h1 class="display-5">Selamat Datang Di Web PHP MVC Saya</h1>
+    <p class="lead">Hallo, Nama Saya <?= $data['nama']; ?>, Pembuat Website MVC Ini.</p>
+    <hr class="my-4">
+    <p>Website CRUD MVC Sederhana BETA TEST</p>
+</div>
+</div>
